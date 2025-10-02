a0338c84c0ce248a9d1b0093261775ee9681fac8
Bitochkin Egor <bigcubecat@yandex.ru>
2025-10-03 00:44:47 +0700

    волны для маяков

--- lib/view/scene/beaconitem.hpp
+++ lib/view/scene/beaconitem.hpp
@@ -3,7 +3,6 @@
 
 #include <QGraphicsEllipseItem>
 #include <QGraphicsSceneMouseEvent>
-#include <QGuiApplication>
 #include <QBrush>
 #include <QString>
 
@@ -11,31 +10,50 @@ class BeaconItem : public QGraphicsEllipseItem {
 public:
     BeaconItem(const QString &name, qreal x, qreal y, qreal radius = 10.0)
         : QGraphicsEllipseItem(-radius, -radius, radius * 2, radius * 2),
-          name_(name)
-    {
+          m_name(name), m_radius(radius) {
         setPos(x, y);
-        setBrush(QBrush(Qt::blue));
+        setPen(Qt::NoPen);
+        setBrush(QBrush(kSecondaryColor[0]));
         updateToolTip();
+        setZValue(7);
+
+        const auto msec = 70;
+        const auto countWaves = 3;
+        const auto r = radius * 7;
+        for (int i = 0; i < countWaves; i++) {
+            auto *waveItem = new WaveItem(
+                radius,
+                r,
+                kSecondaryColor[0],
+                kSecondaryColor[1],
+                this,
+                msec,
+                r * i * msec / countWaves
+            );
+            waveItem->setZValue(8);
+            m_waves.push_back(waveItem);
+        }
     }
 
     void setName(const QString &name) {
-        name_ = name;
+        m_name = name;
         updateToolTip();
     }
 
-    QString name() const { return name_; }
+    QString name() const { return m_name; }
 
 private:
-    QString name_;
+    QString m_name;
+    QList<WaveItem *> m_waves;
+    qreal m_radius;
 
     void updateToolTip() {
         setToolTip(QString("%1 at (%2, %3)")
-                       .arg(name_)
-                       .arg(pos().x())
-                       .arg(pos().y()));
+            .arg(m_name)
+            .arg(pos().x())
+            .arg(pos().y()));
     }
 };
-
 
 
 #endif //APP_BEACONITEM_HPP
